7b048c3403d06039101119b1e5e18eab432a05c9
Laura Rinaldi <rinaldilaurinha@gmail.com>
2023-08-10 15:06:38 -0400

    stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,21 +2,53 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const cart = require("../models/Cart")
 
-router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+router.post("/create-checkout-session", async (req, res) => {
+  console.log(req.body.cart, "hey");
+
+  try {
+    const lineItems = await Promise.all(
+      req.body.cart.products.map(async (prod) => {
+        const product = await stripe.products.create({
+          name: `${prod.product.title}`,
+        });
+        console.log(product);
+        const price = await stripe.prices.create({
+          unit_amount: `${prod.product.price * 100}`,
+          currency: "usd",
+          // recurring: { interval: "month" },
+          product: `${product.id}`,
+        });
+        return {
+          price: price.id,
+          quantity: 1,
+        };
+      })
+    );
+    console.log(lineItems);
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: "http://localhost:5173/success.html",
+      cancel_url: "http://localhost:5173/cancel.html",
+    });
+
+    console.log(session.url);
+    res.json(session.url);
+  } catch (err) {
+    console.log(err);
+  }
+  // const product = await stripe.products.create({
+  //   name: "Gold Special",
+  // });
+  // console.log(product);
+  // const price = await stripe.prices.create({
+  //   unit_amount: 1299,
+  //   currency: "usd",
+  //   recurring: { interval: "month" },
+  //   product: "prod_OQRTUZMzPNqYz2",
+  // });
+  // console.log(price);
 });
 
 module.exports = router;
